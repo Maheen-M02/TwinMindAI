1d1b942fb432291115d3ffb5930ee7d2b9c188e7
Maheencool <maheenmeshram.2115@gmail.com>
2026-05-21 16:25:38 +0530

    Configure Vercel deployment and remove unused frame scroller files

--- twinmind-frontend/src/pages/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import FrameScroller from '../components/FrameScroller'
-
-export default function Landing() {
-  return (
-    <div style={{ background: '#0a0e14', overflowX: 'hidden' }}>
-      <FrameScroller />
-    </div>
-  )
-}
